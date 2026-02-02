1c4d9ae1b35f137c3e3683cc7f734538bcb34e56
Thiago Moraes <tmr@icomp.ufam.edu.br>
2026-02-02 10:42:37 -0400

    feat: new export colors

--- src/tokens/colors.ts
+++ src/tokens/colors.ts
@@ -1,7 +1,9 @@
 export const colorTokens = {
+  // Base
   "base-lt": true,
   "base-dk": true,
 
+  // Neutro
   "neutral-lt-3": true,
   "neutral-lt-2": true,
   "neutral-lt-1": true,
@@ -10,6 +12,7 @@ export const colorTokens = {
   "neutral-dk-2": true,
   "neutral-dk-3": true,
 
+  // Brand
   "brand-lt-2": true,
   "brand-lt-1": true,
   brand: true,
@@ -17,11 +20,44 @@ export const colorTokens = {
   "brand-dk-2": true,
   "brand-dk-3": true,
 
+  // Erro
   "error-lt": true,
   error: true,
 
+  // Sucesso
   "success-lt": true,
   success: true,
+
+  // Backgrounds
+  "bg-lt-2": true,
+  "bg-lt-1": true,
+  bg: true,
+
+  // Especial A
+  "esp-a-lt-1": true,
+  "esp-a": true,
+  "esp-a-dk-1": true,
+
+  // Especial B
+  "esp-b-lt-1": true,
+  "esp-b": true,
+
+  // Especial C
+  "esp-c-lt-2": true,
+  "esp-c-lt-1": true,
+  "esp-c": true,
+
+  // Especial D
+  "esp-d-lt-1": true,
+  "esp-d": true,
+
+  // Especial E
+  "esp-e-lt-1": true,
+  "esp-e": true,
+
+  // Degradê
+  "gradient-start": true,
+  "gradient-end": true,
 } as const;
 
 export type ColorToken = keyof typeof colorTokens;
